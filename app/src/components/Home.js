e9421ded9f26cfcc036197ae2ca34f3c73d0bfb1
Jared Anthony Kronyak <jared.kronyak@gmail.com>
2022-11-15 15:02:27 -0500

    Sample events captures on Home

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 const Home = () => { 
-    
+
+
+
+
     return(
         <div>
-            <p onMouseOver={() => console.log("FOCUSED")}>I am in the Home component!</p>
+            <p>I am in the Home component!</p>
+
+            <p onMouseOver={() => console.log("Hovered")}>Hover over me!</p>
+            
+            <p 
+                onMouseUp={() => console.log("Mouse Up Selection: ", window.getSelection().toString())}
+                onDoubleClick={() => console.log("Double Click Selection: ", window.getSelection().toString())}
+            >
+                Highlight me!
+            </p>
+
+            <p onClick={() => console.log("Clicked")}>Click me!</p>
+            
+
         </div>
     )
 
