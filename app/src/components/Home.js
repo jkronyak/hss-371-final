a77ca03ab61f7f771da73d1dc5a4d1e2e3104ab1
Erik Anhorn <erikjanhorn@gmail.com>
2022-12-01 13:51:46 -0500

    TYPOGRAPHY

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,22 +1,27 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
+import actions from '../actions';
 
 import { Button } from '@mui/material';
 
 const Home = () => { 
-
+    const dispatch = useDispatch();
     const handleMouseOver = (e) => {
         console.log(e);
+        dispatch(actions.addInteraction({type: "HOME_PAGE_MOUSE_HOVER", timestamp: Date.now(), item: null}));
 
     }
 
     const handleButtonClick = (e) => { 
         console.log(e);
+        dispatch(actions.addInteraction({type: "HOME_PAGE_BUTTON_PRESS", timestamp: Date.now(), item: null}));
     }
 
     return(
         <div>
+            <Link to='/results'>Results Test Link</Link>
             <p>I am in the Home component!</p>
             <p>Welcome to our website! Here, you can shop the goods that we are currently offering.</p>
             <p onMouseOver={() => console.log("Hovered")}>Hover over me!</p>
